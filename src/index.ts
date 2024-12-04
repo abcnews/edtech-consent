119819c1b740924a6755e3127d44c07c7b7d713b
Simon Elvery <simon@elvery.net>
2024-12-04 17:02:53 +1000

    Add dev-proxy

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { whenOdysseyLoaded } from '@abcnews/env-utils';
 import { getMountValue, selectMounts } from '@abcnews/mount-utils';
 import type { Mount } from '@abcnews/mount-utils';
 import App from './components/App/App.svelte';
+import { proxy } from '@abcnews/dev-proxy';
 
 let appMountEl: Mount;
 let appProps;
 
-whenOdysseyLoaded.then(() => {
+Promise.all([proxy('edtech-consent'), whenOdysseyLoaded]).then(() => {
   [appMountEl] = selectMounts('edtechconsent');
 
   if (appMountEl) {
